d3c0cb68a43261f5c31b24d5b2001d786e705e34
Arvind-Garg <arvindgarg314@gmail.com>
2026-01-02 23:57:13 +0530

    cascade delete and citext fix

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, String, Integer, Boolean, DateTime, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import CITEXT
 from database import Base
 from datetime import datetime
 
@@ -8,8 +9,8 @@ class User(Base):
     __tablename__= "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), index=True, nullable=False)
-    email = Column(String(255),unique=True, index = True, nullable=False)
+    name = Column(String(255), nullable=False)
+    email = Column(CITEXT, unique=True, index = True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     is_active = Column(Boolean, server_default='True', nullable=False)
     created_at = Column(DateTime, server_default=func.now())
@@ -22,12 +23,12 @@ class Todo(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(300), nullable=False)
+    title = Column(String(255), nullable=False)
     description = Column(Text, nullable=True)
     completed = Column(Boolean, server_default='False', nullable=False)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
 
-    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id, ondelete="CASCADE"), nullable=False)
     owner = relationship("User", back_populates="todos")
 
